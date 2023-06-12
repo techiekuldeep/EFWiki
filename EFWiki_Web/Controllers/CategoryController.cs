ea5a78aec7b8fdb11f5e203b18143d6a7a04b0f7
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-12 12:12:24 +1200

    Retreive using EF Core.

---
+++ EFWiki_Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EFWiki_DataAccess.Data;
+using EFWiki_Model.Models;
+using System.Collections.Generic;
+
+namespace EFWiki_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList(); 
+            return View(objList);
+        }
+    }
+}
