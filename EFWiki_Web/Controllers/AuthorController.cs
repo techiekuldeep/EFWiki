da0d174c018208fa0d039b32f69dc973a97a32a6
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-12 17:02:24 +1200

    Added views for Author and Publisher.

---
+++ EFWiki_Web/Controllers/AuthorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EFWiki_DataAccess.Data;
+using EFWiki_Model.Models;
+using System.Collections.Generic;
+
+namespace EFWiki_Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
