4b0b9ccc4705fd443b797f4c46daa44cea16a836
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-12 17:32:57 +1200

    Added index action method to list All Books & added the index view.

---
+++ EFWiki_Web/Controllers/BookController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EFWiki_DataAccess.Data;
+using EFWiki_Model.Models;
+using System.Collections.Generic;
+
+namespace EFWiki_Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Book> objList = _db.Books.ToList(); 
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj= new();
+            if (id == null || id==0)
+            {
+                //Create
+                return View(obj);
+            }
+            //edit
+            obj = _db.Categories.FirstOrDefault(u=>u.CategoryId==id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.CategoryId == 0)
+                {
+                    //Create
+                    await _db.Categories.AddAsync(obj);
+                }
+                else 
+                {
+                    //Update
+                    _db.Categories.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            Category obj = new();
+            obj = _db.Categories.FirstOrDefault(u => u.CategoryId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
